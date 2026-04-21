501476308134ab7b3d14d2c239ac83fdb35c38b9
SomeYellowGuy <30015613+SomeYellowGuy@users.noreply.github.com>
2026-04-21 22:13:40 +0530

    added `proc-macro-crate` to account for `pumpkin-codecs` using the derive macros of `pumpkin-codecs-macros`
    # Conflicts:
    #	Cargo.lock
    #	pumpkin-codecs-macros/src/lib.rs

--- pumpkin-codecs-macros/src/lib.rs
+++ pumpkin-codecs-macros/src/lib.rs
@@ -2,9 +2,8 @@
 //!
 //! # Attributes
 //!
-//! Every attribute specified here is a sub-attribute; the root attribute must be  `#[codec(...)]`.
+//! Each field in a struct can have a `field` attribute, and it can have the following sub-attributes:
 //!
-//! ## Field Attributes
 //! - `skip`: Skips serializing the field entirely, and instead uses the value provided by `default`, if any.
 //!   In this case, `default` is *optional*, and if it is not specified, this falls back to `Default::default()`.
 //! - `name = "x"`: Sets this field to be encoded with the key `"x"`. If not specified, the field's name
@@ -14,26 +13,22 @@
 //! - `lenient`: Only for encoding `Option`s and defaulted fields. If a value is present and cannot be successfully decoded,
 //!   the value is ignored and a `None`/the default value is decoded instead.
 //!
-//! ## Struct/Enum Body Attributes
-//! - `tag_key = "x"` on `enum`s: Tells the key for storing the enum's type. This is used to differentiate the variant
+//! For enum dispatch, you can also use the following:
+//! - `tag_key("x")` on the `enum`: Tells the key for storing the enum's type. This is used to differentiate the variant
 //!   during decoding. If omitted, this defaults to `"type"`.
-//!
-//! ## Enum Variant Attributes
-//! - `tag = "x"`: Tells the value for storing the enum's type. This is used to differentiate the variant
+//! - `tag("x")` on the `enum` variant: Tells the value for storing the enum's type. This is used to differentiate the variant
 //!   during decoding.
 
-mod attribute;
 mod decode;
 mod encode;
 mod field;
 
-use crate::attribute::{ParsedAttribute, add_attribute_branch};
 use proc_macro::TokenStream;
 use proc_macro_crate::{FoundCrate, crate_name};
 use proc_macro_error2::__export::proc_macro2;
 use proc_macro_error2::__export::proc_macro2::{Ident, Span};
 use quote::{ToTokens, quote};
-use syn::{Attribute, DeriveInput, Error, LitStr, Path, Type, parse_macro_input};
+use syn::{Attribute, DeriveInput, Error, LitStr, Type, parse_macro_input};
 
 /// Returns the tokens corresponding to the `pumpkin_codecs` crate.
 fn crate_token() -> proc_macro2::TokenStream {
@@ -46,12 +41,11 @@ fn crate_token() -> proc_macro2::TokenStream {
 
 /// Derives the `Encode` trait for a struct.
 ///
-/// This trait also derives `MapEncode` (except for enums whose variants are all units and unit structs),
-/// though this trait may only be useful directly for certain cases,
+/// This trait also derives `MapEncode`, though this trait may only be useful for certain cases,
 /// which is then used to derive `Encode`.
 ///
-/// Check the [module's documentation](crate) for every attribute you can use.
-#[proc_macro_derive(Encode, attributes(codec))]
+/// Check the module's documentation for every attribute you can use.
+#[proc_macro_derive(Encode, attributes(field, tag_key, tag))]
 pub fn derive_encode(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     encode::derive_encode(&crate_token(), &input).unwrap_or_else(|e| e.to_compile_error().into())
@@ -59,12 +53,11 @@ pub fn derive_encode(input: TokenStream) -> TokenStream {
 
 /// Derives the `Decode` trait for a struct.
 ///
-/// This trait also derives `MapDecode` (except for enums whose variants are all units and unit structs),
-/// though this trait may only be useful directly for certain cases,
+/// This trait also derives `MapDecode`, though this trait may only be useful for certain cases,
 /// which is then used to derive `Decode`.
 ///
-/// Check the [module's documentation](crate) for every attribute you can use.
-#[proc_macro_derive(Decode, attributes(codec))]
+/// Check the module's documentation for every attribute you can use.
+#[proc_macro_derive(Decode, attributes(field, tag_key, tag))]
 pub fn derive_decode(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     decode::derive_decode(&crate_token(), &input).unwrap_or_else(|e| e.to_compile_error().into())
@@ -81,64 +74,38 @@ fn duplicate_attribute_error(ident: &Ident) -> Error {
     )
 }
 
-fn parse_enum_dispatch_attributes(attributes: &[Attribute]) -> Result<EnumDispatchData, Error> {
-    enum EnumDispatchAttribute {
-        TagKey,
-    }
-
-    impl ParsedAttribute for EnumDispatchAttribute {
-        fn from_path(path: &Path) -> Option<Self> {
-            add_attribute_branch!(path, "tag_key", TagKey);
-            None
-        }
-    }
-
+fn parse_enum_dispatch_attributes(
+    ident: &Ident,
+    attributes: &[Attribute],
+) -> Result<EnumDispatchData, Error> {
     let mut tag_key = None;
-    EnumDispatchAttribute::parse_attributes(attributes, |attribute, meta, ident| {
-        match attribute {
-            // tag_key = "x"
-            EnumDispatchAttribute::TagKey => {
-                if tag_key.is_some() {
-                    return Err(duplicate_attribute_error(ident));
-                }
-                let value = meta.value()?;
-                let lit = value.parse::<LitStr>()?;
-                tag_key = Some(lit.value());
+    for attr in attributes {
+        if attr.path().is_ident("tag_key") {
+            if tag_key.is_some() {
+                return Err(duplicate_attribute_error(ident));
             }
+            let attr: LitStr = attr.parse_args()?;
+            tag_key = Some(attr.value());
         }
-        Ok(())
-    })?;
+    }
     let tag_key = tag_key.unwrap_or("type".to_string());
     Ok(EnumDispatchData { tag_key })
 }
 
-fn parse_enum_variant_attributes(ident: &Ident, attributes: &[Attribute]) -> Result<String, Error> {
-    enum EnumVariantAttribute {
-        Tag,
-    }
-
-    impl ParsedAttribute for EnumVariantAttribute {
-        fn from_path(path: &Path) -> Option<Self> {
-            add_attribute_branch!(path, "tag", Tag);
-            None
-        }
-    }
-
+fn parse_enum_dispatch_variant_attributes(
+    ident: &Ident,
+    attributes: &[Attribute],
+) -> Result<String, Error> {
     let mut ty = None;
-    EnumVariantAttribute::parse_attributes(attributes, |attribute, meta, ident| {
-        match attribute {
-            // tag = "x"
-            EnumVariantAttribute::Tag => {
-                if ty.is_some() {
-                    return Err(duplicate_attribute_error(ident));
-                }
-                let value = meta.value()?;
-                let lit = value.parse::<LitStr>()?;
-                ty = Some(lit.value());
+    for attr in attributes {
+        if attr.path().is_ident("tag") {
+            if ty.is_some() {
+                return Err(duplicate_attribute_error(ident));
             }
+            let attr: LitStr = attr.parse_args()?;
+            ty = Some(attr.value());
         }
-        Ok(())
-    })?;
+    }
     ty.map_or_else(
         || {
             Err(Error::new_spanned(
